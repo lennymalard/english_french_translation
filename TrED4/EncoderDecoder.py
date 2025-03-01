f7345f56dde33f3814d7ed668fa9ac852703e410
Lenny Malard <lennymalard@gmail.com>
2025-03-01 13:25:21 +0100

    Decoder completed

--- TrED4/EncoderDecoder.py
+++ TrED4/EncoderDecoder.py
@@ -20,10 +20,12 @@ class Attention(nn.Module):
         self.head_size = head_size
         self.dropout = nn.Dropout(0.2)
 
-    def forward(self, q, k, v, mask=None):
+    def forward(self, q, k, v, attn_mask=None, pad_mask=None):
         scores = q @ k.transpose(-2, -1)
-        if mask is not None:
-            scores = scores.masked_fill(mask == 0, float('-inf'))
+        if pad_mask is not None:
+            scores = scores.masked_fill(pad_mask == 0, float('-inf'))
+        if attn_mask is not None:
+            scores = scores.masked_fill(attn_mask == 0, float('-inf'))
         attn_weights = F.softmax(scores/sqrt(self.head_size), dim=-1)
         return self.dropout(attn_weights) @ v
 
@@ -45,14 +47,15 @@ class MultiHeadAttention(nn.Module):
         self.dropout = nn.Dropout(0.2)
         self.layer_norm = LayerNorm(embedding_size)
 
-    def forward(self, x, mask=None):
-        seq_length = x.shape[1]
+    def forward(self, q, k, v, attn_mask=None, pad_mask=None):
+        x = q.clone().detach()
+        seq_length = q.shape[1]
 
-        q = self.q_proj(x).contiguous().view(-1, self.num_heads, seq_length, self.head_size)
-        k = self.k_proj(x).contiguous().view(-1, self.num_heads, seq_length, self.head_size)
-        v = self.v_proj(x).contiguous().view(-1, self.num_heads, seq_length, self.head_size)
+        q = self.q_proj(q).contiguous().view(-1, self.num_heads, seq_length, self.head_size)
+        k = self.k_proj(k).contiguous().view(-1, self.num_heads, seq_length, self.head_size)
+        v = self.v_proj(v).contiguous().view(-1, self.num_heads, seq_length, self.head_size)
 
-        output = self.attention(q, k, v, mask=mask).reshape(-1, seq_length, self.embedding_size)
+        output = self.attention(q, k, v, attn_mask=attn_mask, pad_mask=pad_mask).reshape(-1, seq_length, self.embedding_size)
         return self.layer_norm(x + self.projection(self.dropout(output)))
 
 class FeedForward(nn.Module):
@@ -93,15 +96,39 @@ class Encoder(nn.Module):
             ) for _ in range(num_layers)
         ])
 
-    def forward(self, x, padding_mask=None):
-        x = self.embedding(x)
-        x = self.positional_encoding(x)
+    def forward(self, input, attn_mask=None, pad_mask=None):
+        input = self.embedding(input)
+        input = self.positional_encoding(input)
         for layer in self.layers:
-            x = layer(x)
-        return x
+            mha, ffn = layer
+            input = mha(input, input, input, attn_mask=attn_mask, pad_mask=pad_mask)
+            input = ffn(input)
+        return input
 
 class Decoder(nn.Module):
-    pass
+    def __init__(self, vocab_size, num_layers):
+        super().__init__()
+        self.embedding = nn.Embedding(num_embeddings=vocab_size, embedding_dim=512)
+        self.positional_encoding = PositionalEncoding()
+        self.linear = nn.Linear(512, vocab_size)
+        self.layers = nn.ModuleList([
+            nn.Sequential(
+                MultiHeadAttention(embedding_size=512, num_heads=8),
+                MultiHeadAttention(embedding_size=512, num_heads=8),
+                FeedForward(embedding_size=512, output_size=2048)
+            ) for _ in range(num_layers)
+        ])
+
+    def forward(self, input, encoder_output, attn_mask=None, enc_pad_mask=None, dec_pad_mask=None):
+        input = self.embedding(input)
+        input = self.positional_encoding(input)
+        for layer in self.layers:
+            mha1, mha2, ffn = layer
+            input = mha1(input, input, input, attn_mask=attn_mask, pad_mask=dec_pad_mask)
+            input = mha2(input, encoder_output, encoder_output, pad_mask=enc_pad_mask)
+            input = ffn(input)
+        input = self.linear(input)
+        return input
 
 class EncoderDecoder(nn.Module):
     pass
